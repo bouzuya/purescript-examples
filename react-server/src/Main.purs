c4234420123afdd75ce5eb6daeb96211900d5e0d
bouzuya <m@bouzuya.net>
2016-12-26 16:59:07 +0900

    html

--- react-server/src/Main.purs
+++ react-server/src/Main.purs
@@ -4,8 +4,8 @@ import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Prelude (Unit, ($))
 import React (ReactElement, createElementTagName)
-import React.DOM (div, text)
-import React.DOM.Props (className)
+import React.DOM (body, div, head, html, meta, text, title)
+import React.DOM.Props (charSet, className, lang)
 import ReactDOM (renderToStaticMarkup)
 
 -- <div class="message">Hello, React!</div>
@@ -15,8 +15,21 @@ message = div
   [ text "Hello, React!" ]
 
 -- <div>{message}</div>
+app :: ReactElement
+app = createElementTagName "div" {} [ message ]
+
+-- <html lang="ja">...<body>{app}</body></html>
 root :: ReactElement
-root = createElementTagName "div" {} [message]
+root =
+  html
+    [ lang "ja" ]
+    [ head []
+           [ meta [ charSet "utf-8" ] []
+           , title [] [ text "TITLE" ]
+           ]
+    , body []
+           [ app ]
+    ]
 
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
