d016c85824977556c2b061889645810f0197dcb3
bouzuya <m@bouzuya.net>
2016-12-26 16:15:21 +0900

    React.DOM

--- react-server/src/Main.purs
+++ react-server/src/Main.purs
@@ -1,14 +1,22 @@
 module Main where
 
-import Prelude
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
+import Prelude (Unit, ($))
 import React (ReactElement, createElementTagName)
+import React.DOM (div, text)
+import React.DOM.Props (className)
 import ReactDOM (renderToStaticMarkup)
 
--- <div></div>
+-- <div class="message">Hello, React!</div>
+message :: ReactElement
+message = div
+  [ className "message" ]
+  [ text "Hello, React!" ]
+
+-- <div>{message}</div>
 root :: ReactElement
-root = createElementTagName "div" {} []
+root = createElementTagName "div" {} [message]
 
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
