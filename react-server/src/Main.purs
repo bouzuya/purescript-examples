98c66ef98206a9802cd130948eb413a82bc4c23c
bouzuya <m@bouzuya.net>
2016-12-26 18:25:47 +0900

    stateless component

--- react-server/src/Main.purs
+++ react-server/src/Main.purs
@@ -2,11 +2,18 @@ module Main where
 
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
-import Prelude (Unit, ($))
-import React (ReactElement, createElementTagName)
-import React.DOM (body, div, head, html, meta, text, title)
+import Prelude (Unit, ($), (<>))
+import React (ReactClass, ReactElement, createClassStateless, createElement, createElementTagName)
+import React.DOM (body, div, h1, head, html, meta, text, title)
 import React.DOM.Props (charSet, className, lang)
 import ReactDOM (renderToStaticMarkup)
+
+-- <h1>Hello, {name}</h1>
+welcomeClass :: ReactClass { name :: String }
+welcomeClass = createClassStateless f
+  where
+    f :: { name :: String } -> ReactElement
+    f { name } = h1 [] [ text $ "Hello, " <> name ]
 
 -- <div class="message">Hello, React!</div>
 message :: ReactElement
@@ -14,9 +21,14 @@ message = div
   [ className "message" ]
   [ text "Hello, React!" ]
 
--- <div>{message}</div>
+-- <div>{welcome}{message}</div>
 app :: ReactElement
-app = createElementTagName "div" {} [ message ]
+app = createElementTagName
+  "div"
+  {}
+  [ message
+  , createElement welcomeClass { name: "Sara" } []
+  ]
 
 -- <html lang="ja">...<body>{app}</body></html>
 root :: ReactElement
