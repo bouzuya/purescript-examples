c34b0c14a56bed23ae6811eaf60b5c8ca71175e5
bouzuya <m@bouzuya.net>
2017-02-09 08:32:00 +0900

    add Node.Stream.readEither ver.

--- stdin/src/Main.purs
+++ stdin/src/Main.purs
@@ -1,25 +1,45 @@
 module Main where
 
 import Control.Monad.Aff (Aff, makeAff, runAff)
-import Control.Monad.Eff (Eff)
+import Control.Monad.Eff (Eff, runPure)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Control.Monad.Eff.Exception (EXCEPTION)
-import Data.Maybe (Maybe(..), fromMaybe)
+import Control.Monad.Eff.Unsafe (unsafeCoerceEff)
+import Data.Either (Either(Left, Right))
+import Data.Maybe (Maybe(..))
+import Node.Buffer (Buffer, toString) as Buffer
 import Node.Encoding (Encoding(..)) as Encoding
 import Node.Process (stdin) as Process
-import Node.Stream (Readable, onReadable, readString) as Stream
+import Node.Stream (Readable, onReadable, readEither) as Stream
 import Prelude (Unit, (<$>), ($), bind, const, pure, unit, void)
 
-readStdin
+toString
+  :: Encoding.Encoding
+  -> Maybe (Either String Buffer.Buffer)
+  -> String
+toString _ Nothing = "" -- ignore `null`
+toString _ (Just (Left s)) = s
+toString e (Just (Right b)) = (runPure $ unsafeCoerceEff $ Buffer.toString e b) -- ?
+
+readStdin'
   :: forall e
-   . Stream.Readable () ( err :: EXCEPTION | e )
-  -> Aff ( err :: EXCEPTION | e ) String
-readStdin stdin = makeAff \_ ok -> do
+   . Stream.Readable () e
+  -> Aff e String
+readStdin' stdin = makeAff \_ ok -> do
   Stream.onReadable stdin do
-    s <- fromMaybe "" <$> Stream.readString stdin Nothing Encoding.UTF8
+    s <- toString Encoding.UTF8 <$> Stream.readEither stdin Nothing
     ok s
+
+-- readStdin
+--    :: forall e
+--    . Stream.Readable () ( err :: EXCEPTION | e )
+--    -> Aff ( err :: EXCEPTION | e ) String
+--  readStdin stdin = makeAff \_ ok -> do
+--    Stream.onReadable stdin do
+--      s <- fromMaybe "" <$> Stream.readString stdin Nothing Encoding.UTF8
+--      ok s
 
 main
   :: forall eff
    . Eff ( console :: CONSOLE, err :: EXCEPTION | eff ) Unit
-main = void $ runAff (const (pure unit)) (\s -> log s) $ readStdin Process.stdin
+main = void $ runAff (const (pure unit)) (\s -> log s) $ readStdin' Process.stdin
