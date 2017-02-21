3604158c2888772ad78a138027caf417220ba0ad
bouzuya <m@bouzuya.net>
2017-02-22 08:08:52 +0900

    halogen-server

---
+++ halogen-server/README.md
@@ -0,0 +1,11 @@
+# react-server
+
+- `purescript-react` [purescript-contrib/purescript-react][]
+- https://pursuit.purescript.org/packages/purescript-react/2.0.0
+- `purescript-react-dom` [purescript-contrib/purescript-react-dom][]
+- https://pursuit.purescript.org/packages/purescript-react-dom/2.0.0
+- [PureScript by Example 8. The Eff Monad][purescript-by-example-8]
+
+[purescript-by-example-8]: https://leanpub.com/purescript/read#leanpub-auto-the-eff-monad
+[purescript-contrib/purescript-react]: https://github.com/purescript-contrib/purescript-react
+[purescript-contrib/purescript-react-dom]: https://github.com/purescript-contrib/purescript-react-dom
---
+++ halogen-server/src/Main.purs
@@ -0,0 +1,26 @@
+module Main where
+
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Console (CONSOLE, log)
+import Halogen.HTML (HTML, body, div_, head, html, meta, text, title)
+import Halogen.HTML.Properties (charset) as P
+import Halogen.HTML.Renderer.String (renderHTML) as R
+import Prelude (Unit, ($))
+
+-- <html>...<body><div>HELLO</div></body></html>
+root :: forall p i. HTML p i
+root =
+  html
+    []
+    [ head []
+           [ meta [ P.charset "utf-8" ]
+           , title [] [ text "TITLE" ]
+           ]
+    , body []
+           [ div_ [ text "HELLO" ]
+           ]
+    ]
+
+main :: forall e. Eff (console :: CONSOLE | e) Unit
+main = do
+  log $ R.renderHTML root
