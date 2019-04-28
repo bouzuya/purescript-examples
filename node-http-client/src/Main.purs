04d67c2130a933b5a29984c1188851dbd7c77e8d
noolbar <noolbar@outlook.com>
2019-04-29 03:06:24 +0900

    update node-http-client #2

--- node-http-client/src/Main.purs
+++ node-http-client/src/Main.purs
@@ -1,14 +1,13 @@
 module Main where
 
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log, logShow)
-import Control.Monad.Eff.Exception (EXCEPTION)
+import Prelude
+
 import Data.Options (Options, assoc)
+import Effect (Effect)
+import Effect.Class.Console (logShow)
 import Node.Encoding (Encoding(..))
-import Node.HTTP (HTTP)
 import Node.HTTP.Client (RequestOptions, hostname, method, path, port, protocol, request, requestAsStream, responseAsStream, responseHeaders, statusCode)
 import Node.Stream (end, onDataString)
-import Prelude (Unit, ($), (<>), bind, pure, unit)
 
 options :: Options RequestOptions
 options =
@@ -18,11 +17,11 @@ options =
   assoc method "GET" <>
   assoc path "/posts.json"
 
-main :: forall e. Eff (err :: EXCEPTION, console :: CONSOLE, http :: HTTP | e) Unit
+main :: Effect Unit
 main = do
   req <- request options \res -> do
     logShow $ statusCode res
     logShow $ responseHeaders res
-    onDataString (responseAsStream res) UTF8 log
+    onDataString (responseAsStream res) UTF8 logShow
   end (requestAsStream req) (pure unit)
-  log "Hello sailor!"
+  logShow "Hello sailor!"
